aee23de77dfdb08560893dd446e91508ef0168b7
Alessandre Lopes <alessandre.lopesc@gmail.com>
2021-11-24 12:08:40 -0400

    Aula1

---
+++ src/br/com/sisgti/Aula1.java
@@ -0,0 +1,9 @@
+package br.com.sisgti;
+
+public class Aula1 {
+    public static void main(String[] args){
+
+        String a = "Alessandre Lopes";
+        System.out.println("Ola Mundo, sou o: " + a);
+    }
+}
