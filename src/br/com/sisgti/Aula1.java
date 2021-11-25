85c5f566a46dd138db9d2347b8521105bf7e34a3
Alessandre Lopes <alessandre.lopesc@gmail.com>
2021-11-25 12:03:41 -0400

    aula 2 - criando variaveis

--- src/br/com/sisgti/Aula1.java
+++ src/br/com/sisgti/Aula1.java
@@ -4,6 +4,12 @@ public class Aula1 {
     public static void main(String[] args){
 
         String a = "Alessandre Lopes";
+        int a1 = 5;
+        int b1 =6;
+        int resultado = a1+b1;
+
         System.out.println("Ola Mundo, sou o: " + a);
+
+        System.out.println("Somando valor de variaveis " + resultado);
     }
 }
